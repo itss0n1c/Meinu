c0007cbc77dd4b528f76f115335a751cb9252f1f
S0n1c <itss0n1cdev@gmail.com>
2021-08-26 18:19:41 -0400

    remove extra commands

--- src/cmds/index.ts
+++ src/cmds/index.ts
@@ -1,7 +1,5 @@
-import cm from './cm';
-import color from './color';
 import echo from './echo';
 import evalc from './eval';
 import help from './help';
 
-export const defaultCommands = [ help, echo, evalc, color, cm ];
+export const defaultCommands = [ help, echo, evalc ];
